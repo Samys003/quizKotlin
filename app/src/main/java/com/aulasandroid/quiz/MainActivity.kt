82a16d1fa263caaccc84cae30e42bb4765ed349d
25132563 <samara_rolim010@hotmail.com>
2026-03-23 11:28:08 -0300

    criando repositorio, game de quiz

---
+++ app/src/main/java/com/aulasandroid/quiz/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.aulasandroid.quiz
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.aulasandroid.quiz.Screens.initScreen
+import com.aulasandroid.quiz.ui.theme.QuizTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            QuizTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    initScreen(modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+
---
+++ app/src/main/java/com/aulasandroid/quiz/Screens/initScreen.kt
@@ -0,0 +1,17 @@
+package com.aulasandroid.quiz.Screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun initScreen(modifier: Modifier = Modifier) {
+    Column( modifier = Modifier.fillMaxSize()
+        .background(color = Color(36, 189, 185, 255))
+    ) {
+
+    }
+}
